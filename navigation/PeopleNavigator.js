8fe8cbd6a086ec80176d4a5acc656ea9d6519b88
lachlandixon2 <lachlan.dixon2@studytafensw.edu.au>
2023-11-13 13:05:13 +1100

    new

---
+++ navigation/PeopleNavigator.js
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+
+// Import navigation and screens
+import ViewPeopleScreen from '../screens/ViewPeopleScreen';
+
+// Import styling and components
+import Styles from "../styles/MainStyle";
+
+const Stack = createNativeStackNavigator();
+
+export default function PeopleNavigator() {
+  return (
+    <Stack.Navigator 
+      initialRouteName="ViewPeople"
+      screenOptions={{
+        headerShown: true,
+        headerBackTitle: "Back",
+        headerStyle: Styles.headerBar,
+        headerTitleStyle: Styles.headerBarTitle,
+      }}>
+      <Stack.Screen
+        name="ViewPeople"
+        component={ViewPeopleScreen}
+        options={{ title: 'View All People' }} />
+    </Stack.Navigator>
+  );
+}
